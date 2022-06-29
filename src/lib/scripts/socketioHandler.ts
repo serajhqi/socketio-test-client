b5e3afca3ad870ab77cc4543c9fca7fedbf3620f
SERAJ <petrichor7u@gmail.com>
2022-06-29 22:29:28 +0430

    feat: sockethandler sendrequest

--- src/lib/scripts/socketioHandler.ts
+++ src/lib/scripts/socketioHandler.ts
@@ -1,44 +1,48 @@
-
 import { io, Socket } from "socket.io-client";
-import { serverAddress, connectionStatus } from '../store';
-import { get } from 'svelte/store';
+import { serverAddress, connectionStatus } from "../store";
+import { get } from "svelte/store";
+import { saveRequest, removeRequest } from "./storageHandler";
 
-let socket: Socket | null = null;
-export const toggleConnection =  async() => {
-    try{
-        let state = get(connectionStatus);
-        let address = get(serverAddress);
-
-        if(state == 'disconnected' || state == 'disconnecting'){
-          connectionStatus.set('connecting')
-          socket = io(address);
-          socket.connect();
-          
-          socket.on('connect', () => {
-            console.log('5')
-            connectionStatus.set('connected')
-          });
-
-          socket.on('disconnect', () => {
-            console.log('6')
-            connectionStatus.set('disconnected')
-          });
-         
-        }else if(state== 'connected' || state == 'connecting'){     
-
-          connectionStatus.set('disconnecting')
-          socket?.disconnect()
-          socket?.close();
-          connectionStatus.set('disconnected');
-          socket = null;
-
-        }
-        
-        
-    }catch(e){
-      console.log('4')
-
-        console.log(e)
-    }
+const isJson = (str: string) => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
 }
 
+let socket: Socket | null = null;
+export const toggleConnection = () => {
+  try {
+    let state = get(connectionStatus);
+    let address = get(serverAddress);
+
+    if (state == "disconnected" || state == "disconnecting") {
+      connectionStatus.set("connecting");
+
+      socket = io(address, { timeout: 5000 });
+      socket.connect();
+      socket.on("connect", () => connectionStatus.set("connected"));
+      socket.on("disconnect", () => connectionStatus.set("disconnected"));
+    } else if (state == "connected" || state == "connecting") {
+      connectionStatus.set("disconnecting");
+
+      socket?.disconnect();
+      socket?.close();
+
+      connectionStatus.set("disconnected");
+      socket = null;
+    }
+  } catch (e) {}
+};
+
+export const sendRequest = (request:string, emitName:string, title: string, loading: boolean) => {
+  if (!socket) return false;
+  const requestJson = isJson(request) ? JSON.parse(request) : request;
+  loading = true;
+  socket.emit(emitName, requestJson, (response: any) => {
+    loading = false;
+    saveRequest(request, response, emitName, title);
+  });
+}
