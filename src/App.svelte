eba4bf7df3bb5810bc67ae915869cac9ff9b22bb
SERAJ <petrichor7u@gmail.com>
2022-06-29 21:13:22 +0430

    feat: Notification structure applied

--- src/App.svelte
+++ src/App.svelte
@@ -2,24 +2,32 @@
   import "./lib/tailwind.css";
   import ToolBar from "./lib/components/ToolBar.svelte";
   import Request from "./lib/components/Request.svelte";
+  import { onMount } from "svelte";
+  import { serverAddress } from "./lib/store";
+  import Notifications from 'svelte-notifications';
+  
+  onMount(()=>{
+    serverAddress.set(localStorage.getItem('socketio.address') || null);
+  })
 </script>
 
-<main class="w-screen h-screen bg-stone-700">
-    <ToolBar/>
+<Notifications>
+  <main class="w-screen h-screen bg-stone-700">
+      <ToolBar/>
 
-    <div class="flex flex-col md:flex-row w-full">
-      <div class="md:w-1/5 w-full">
-        history
+      <div class="flex flex-col md:flex-row w-full">
+        <div class="md:w-1/5 w-full">
+          history
+        </div>
+        <div class="md:w-2/5 w-full h-full border-2 border-gray-400">
+          <Request/>
+        </div>
+        <div class="md:w-2/5 w-full">
+          response
+        </div>
       </div>
-      <div class="md:w-2/5 w-full h-full border-2 border-gray-400">
-        <Request/>
-      </div>
-      <div class="md:w-2/5 w-full">
-        response
-      </div>
-    </div>
-</main>
-
+  </main>
+</Notifications>
 
 <style>
 
