0179f02031349b4fa18f3cb91e5018a58fe2f071
SERAJ <petrichor7u@gmail.com>
2022-07-03 03:35:39 +0430

    feat: tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+      extend: { 
+        colors:{
+          burnt: '#2b2b2b',
+          semiburnt: '#958686',
+          burning: '#343434'
+      }
+    }
   },
   plugins: []
 }
