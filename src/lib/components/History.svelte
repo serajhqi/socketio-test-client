337e0ca4259c3eb20292a545aa025f67969bec0a
SERAJ <petrichor7u@gmail.com>
2022-07-03 13:49:02 +0430

    fix: history rename title

--- src/lib/components/History.svelte
+++ src/lib/components/History.svelte
@@ -23,7 +23,7 @@ import { createEventDispatcher } from "svelte";
 
 <div class="min-w-500">
     <div class="flex flex-row justify-between px-2 py-2 h-10 text-left text-sm text-semiburnt border-b border-burning w-full shadow-b-lg">
-        <input placeholder="Search" class="bg-transparent outline-none focus:text-white focus:placeholder-gray-300" bind:value={search}/>
+        <input placeholder="Search Requsets" class="bg-transparent outline-none focus:text-white focus:placeholder-gray-300" bind:value={search}/>
         <button on:click={collapse}>
             <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> 
         </button>
